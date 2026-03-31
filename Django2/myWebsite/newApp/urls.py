f7decc6ade13dc073e634bed8928b0101be07fad
adiptachanda05 <adiptachanda05@gmail.com>
2026-03-31 14:20:29 +0530

    Delete Django2 directory

--- Django2/myWebsite/newApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.app,name='app'),
-    path('about/',views.about,name='about'),
-    path('contacts/',views.contacts,name='contacts'),
-   
-]
